a18423eb3d97b04972387f368052349e33230ce5
Hannaii <139317302+Hannaii@users.noreply.github.com>
2024-08-14 14:08:25 +0000

    selesai chapter 3

--- app/layout.js
+++ app/layout.js
@@ -1,11 +1,11 @@
-// /app/layout.js
-
 import "@/app/ui/global.css";
+import { lusitana } from "./ui/fonts";
 
+// /app/layout.jsx
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <body>{children}</body>
+      <body className={`${lusitana.className} antialiased`}>{children}</body>
     </html>
   );
 }
